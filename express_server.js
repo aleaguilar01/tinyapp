e61fbd4042543e27a5f45de79e5ca8ef46f28ca4
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-27 22:40:54 -0600

    Add a second route for each ID and template.

--- express_server.js
+++ express_server.js
@@ -9,15 +9,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
 app.get("/urls", (req, res) => {
   const urls = { urls: urlDatabase};
   res.render("urls_index", urls);
+});
+
+app.get(`/urls/:id`, (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  res.render("urls_show.ejs", {id, longURL});
 });
 
 app.get("/hello", (req, res) => {
