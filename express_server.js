6a7305180d82f3627c02a012c9825b947df2b009
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-28 11:01:20 -0600

    Add post method for urls

--- express_server.js
+++ express_server.js
@@ -14,8 +14,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 /**
