7cb8e7f182693fcd58b1a004ecf1b77207b9f5b3
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-04-02 22:09:02 -0600

    Add post route for /register and update all endpoints to pass entire user instead of username.

--- express_server.js
+++ express_server.js
@@ -3,23 +3,49 @@
  * @author Alejandra Aguilar
  */
 
+/**
+ * Dependencies
+ */
 const express = require("express");
 const cookieParser = require("cookie-parser");
+
+/**
+ * Express initialization
+ */
 const app = express();
 const PORT = 8080; // default port 8080
 
 //configuration for view engine as ejs
 app.set("view engine", "ejs");
 
-// encoding for urls
-app.use(express.urlencoded({ extended: true }));
+/**
+ * Middleware
+ */
+app.use(express.urlencoded({ extended: true })); // encoding for urls
 app.use(cookieParser());
 
+/**
+ * urls and tinyurls database
+ */
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+/**
+ * users database
+ */
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 /**
  * Function to generate a random string thar will be use as a unique identifier for tinyurls
  * @param {number} lenghtOfId
@@ -84,12 +110,32 @@ app.post("/logout", (req, res) => {
 });
 
 /**
+ * Endpoint to update the users database.
+ */
+app.post("/register", (req, res) => {
+  const userId = generateRandomString(6);
+  users[userId] = {
+    id: userId,
+    email: req.body.email,
+    password: req.body.password
+  };
+  res.cookie('user_id', userId);
+  res.redirect(`/urls`);
+});
+
+/**
  * Endpoint to fetch all urls saved in the database
  */
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
-  if (req.cookies["username"]) {
-    templateVars["username"] = req.cookies["username"];
+  const templateVars = {
+    urls: urlDatabase,
+  };
+  if (req.cookies["user_id"]) {
+    const userID = req.cookies["user_id"];
+    const user = users[userID];
+    if (user) {
+      templateVars.user = user;
+    }
   }
   res.render("urls_index", templateVars);
 });
@@ -99,8 +145,12 @@ app.get("/urls", (req, res) => {
  */
 app.get("/urls/new", (req, res) => {
   const templateVars = {};
-  if (req.cookies["username"]) {
-    templateVars["username"] = req.cookies["username"];
+  if (req.cookies["user_id"]) {
+    const userID = req.cookies["user_id"];
+    const user = users[userID];
+    if (user) {
+      templateVars.user = user;
+    }
   }
   res.render("urls_new", templateVars);
 });
@@ -114,8 +164,12 @@ app.get(`/urls/:id`, (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
   const templateVars = { id, longURL };
-  if (req.cookies["username"]) {
-    templateVars["username"] = req.cookies["username"];
+  if (req.cookies["user_id"]) {
+    const userID = req.cookies["user_id"];
+    const user = users[userID];
+    if (user) {
+      templateVars.user = user;
+    }
   }
   res.render("urls_show.ejs", templateVars);
 });
@@ -127,8 +181,12 @@ app.get("/u/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
   const templateVars = {};
-  if (req.cookies["username"]) {
-    templateVars["username"] = req.cookies["username"];
+  if (req.cookies["user_id"]) {
+    const userID = req.cookies["user_id"];
+    const user = users[userID];
+    if (user) {
+      templateVars.user = user;
+    }
   }
   res.redirect(longURL);
 });
@@ -138,12 +196,6 @@ app.get("/u/:id", (req, res) => {
  */
 app.get("/register", (req, res) => {
   res.render("register.ejs");
-});
-
-
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
